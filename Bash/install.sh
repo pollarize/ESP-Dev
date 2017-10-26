eb401fc40e5c6e27377e5afb790fe7597caac617
Mihail Maldzhanski <tribal@email.bg>
2017-10-27 00:23:20 +0300

    Add All in One script
    The script has purpose to install all needed files for ESP8266 and ESP32 ont Linux based OS.

---
+++ Bash/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Welcome to ESP-SDK installation\n\n"
+
+
+echo "The following script will setup the following thing:"
+
+echo "####################################################"
+echo "# 1.ESP8266-Development toolchain files            #" 
+echo "# 2.ESP8266-Official development kit with RTOS     #"
+echo "# 3.ESP32-Prebuilt binaries used from SDK          #"
+echo "# 4.ESP-IDF Latest SDK files                       #"
+echo "####################################################"
